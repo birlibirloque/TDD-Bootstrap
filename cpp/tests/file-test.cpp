303fbc6361825c438e5e6d8484685e8f2bf1696f
birlibirloque <manuel.kstro@gmail.com>
2019-11-26 21:47:24 +0100

    First commit

---
+++ cpp/tests/file-test.cpp
@@ -0,0 +1,7 @@
+#include "file.h"
+#include "catch.hpp"
+
+TEST_CASE("one_yields_itself")
+{
+    FAIL("Ok! - Now let's go with one test");
+}
---
+++ java/src/test/java/ClassTest.java
@@ -0,0 +1,11 @@
+import static org.junit.jupiter.api.Assertions.fail;
+
+import org.junit.jupiter.api.Test;
+
+public class ClassTest {
+
+    @Test public void
+    should_return_somethings_if_somethings_happends() {
+        fail();
+    }
+}
